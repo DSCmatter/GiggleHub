b82ae0120cc92f3be768ccaa0144e7e7e4c6b621
DSCmatter <136594818+DSCmatter@users.noreply.github.com>
2024-07-16 20:13:31 +0530

    changes

---
+++ script.mjs
@@ -0,0 +1,49 @@
+// jokes.js
+
+const apiKey = import.meta.env.API_Key
+
+export async function getJoke() {
+  const selectedCategories = Array.from(document.querySelectorAll('input[name="category"]:checked')).map(checkbox => checkbox.value);
+  
+  if (selectedCategories.length > 0) {
+    const categoryParam = selectedCategories.join(',');
+    const url = ' https://v2.jokeapi.dev/joke/Any';
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': apiKey,
+        'x-rapidapi-host': 'jokeapi-v2.p.rapidapi.com',
+        'User-Agent': 'GiggleHub'
+      }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      if (result.error) {
+        throw new Error(result.message);
+      }
+      displayJoke(result);
+    } catch (error) {
+      console.error(error);
+      displayError(error.message);
+    }
+  } else {
+    displayError('Please select at least one joke category.');
+  }
+}
+
+function displayJoke(jokeData) {
+  const jokeDisplay = document.getElementById('jokeDisplay');
+  jokeDisplay.innerHTML = '';
+  if (jokeData.type === 'single') {
+    jokeDisplay.innerHTML = `<p class="lead">${jokeData.joke}</p>`;
+  } else {
+    jokeDisplay.innerHTML = `<p class="lead">${jokeData.setup}</p><p class="fw-bold">${jokeData.delivery}</p>`;
+  }
+}
+
+function displayError(errorMessage) {
+  const jokeDisplay = document.getElementById('jokeDisplay');
+  jokeDisplay.innerHTML = `<div class="alert alert-danger">${errorMessage}</div>`;
+}
---
+++ vite.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Configuration options
+  root: 'E:\\github\\GiggleHub',  // Optional: specify the root directory of your project
+  server: {
+    port: 3000,  // Optional: specify the port for the dev server
+  },
+  // You can add more configuration options here
+})
